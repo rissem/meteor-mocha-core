d9eefd59551f4fb229b827f5468dd50f72349718
Ronen Babayoff <ronen@lavaina.com>
2015-08-03 20:10:01 -0400

    Update package summary

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'practicalmeteor:mocha-core',
-  summary: "Fibers aware mocha server side wrappers.",
+  summary: "Fibers aware mocha server side wrappers. Internal package - use practicalmeteor:mocha.",
   version: "0.1.1",
   debugOnly: true,
   git: "https://github.com/practicalmeteor/meteor-mocha-core.git"
