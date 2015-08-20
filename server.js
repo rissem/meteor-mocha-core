ca304319c4f1bc90c85541b601f0b30a3f210c04
Juan Sepulveda <juan@lavaina.com>
2015-08-20 10:55:13 -0400

    Refactor and DRY

--- server.js
+++ server.js
@@ -48,81 +48,70 @@ setupGlobals = function(mocha){
     };
   };
 
-  // Wrap mocha hooks and apply mocha suite context
-  var wrappedFunc = function (func) {
-    //  This is the function that mocha will run
-    // the 'this' context is mocha suite context
-    return function (callback) {
 
-      if (func.length == 0) {
-          func.call(this);
-          callback();
-        }
-        else {
-        func.call(this, callback);
-        }
-      }
-    };
 
   var mochaExports = {};
   mocha.suite.emit("pre-require", mochaExports, undefined, mocha);
   //console.log(mochaExports);
 
-  // 1. patch up describe function so it plays nice w/ fibers
+  // 1. patch up it and hooks functions so it plays nice w/ fibers
   // 2. trick to allow binding the suite instance as `this` value
-  // inside of describe blocks, to allow e.g. to set custom timeouts.
-  function wrapRunnable(func) {
-    return function() {
-      this.toString = function(){return func.toString()};
-      // `this` will be bound to the suite instance, as of Mocha's `describe` implementation
-      moddedBindEnvironment(func.bind(this), function(err) { throw err; })();
-    }
-  }
+  // inside of suites blocks, to allow e.g. to set custom timeouts.
+  var wrapRunnable = function (func) {
+
+    //In Meteor, these blocks will all be invoking Meteor code and must
+    //run within a fiber. We must therefore wrap each with something like
+    //bindEnvironment. The function passed off to mocha must have length
+    //greater than zero if we want mocha to run it asynchronously. That's
+    //why it uses the moddedBindEnvironment function described above instead
+
+    //We're actually having mocha run all tests asynchronously. This
+    //is because mocha cannot tell when a synchronous fiber test has
+    //finished, because the test runner runs outside a fiber.
+
+    //It is possible that the mocha test runner could be run from within a
+    //fiber, but it was unclear to me how that could be done without
+    //forking mocha itself.
+
+    var wrappedFunc = function (callback) {
+
+      if (func.length == 0) {
+        func.call(this);
+        callback();
+      }
+      else {
+        func.call(this, callback);
+      }
+    };
+
+    var boundWrappedFunction = moddedBindEnvironment(wrappedFunc, function(err){
+      throw err;
+    });
+
+    // Show original function source code
+    boundWrappedFunction.toString = function(){return func.toString()};
+    return boundWrappedFunction
+  };
 
   global.describe = function (name, func){
-    return mochaExports.describe(name, wrapRunnable(func));
+    return mochaExports.describe(name, moddedBindEnvironment(func.bind(this), function(err) { throw err; }));
   };
   global.describe.skip = mochaExports.describe.skip;
   global.describe.only = function(name, func) {
-    mochaExports.describe.only(name, moddedBindEnvironment(func, function(err){ throw err; }));
+    mochaExports.describe.only(name, moddedBindEnvironment(func.bind(this), function(err) { throw err; }));
   };
 
-  //In Meteor, these blocks will all be invoking Meteor code and must
-  //run within a fiber. We must therefore wrap each with something like
-  //bindEnvironment. The function passed off to mocha must have length
-  //greater than zero if we want mocha to run it asynchronously. That's
-  //why it uses the moddedBindEnivronment function described above instead
-
-  //We're actually having mocha run all tests asynchronously. This
-  //is because mocha cannot tell when a synchronous fiber test has
-  //finished, because the test runner runs outside a fiber.
-
-  //It is possible that the mocha test runner could be run from within a
-  //fiber, but it was unclear to me how that could be done without
-  //forking mocha itself.
-
   global['it'] = function (name, func){
-
-    boundWrappedFunction = moddedBindEnvironment(wrappedFunc(func), function(err){
-      throw err;
-    });
-    // Show original function source code
-    boundWrappedFunction.toString = function(){ return func.toString()};
-
-    mochaExports['it'](name, boundWrappedFunction);
+    mochaExports['it'](name, wrapRunnable(func));
   };
   global.it.skip = mochaExports.it.skip;
   global.it.only = function(name, func) {
-    mochaExports.it.only(name, moddedBindEnvironment(wrappedFunc(func), function(err){ throw err; }));
+    mochaExports.it.only(name, wrapRunnable(func));
   };
 
   ["before", "beforeEach", "after", "afterEach"].forEach(function(testFunctionName){
     global[testFunctionName] = function (func){
-
-      boundWrappedFunction = moddedBindEnvironment(wrappedFunc(func), function(err){
-        throw err;
-      });
-      mochaExports[testFunctionName](boundWrappedFunction);
+      mochaExports[testFunctionName](wrapRunnable(func));
     }
   });
-}
+};
