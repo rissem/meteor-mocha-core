2b086c86a7bc0e799e076b540b6766bd1b3cd02f
Juan Sepulveda <juan@lavaina.com>
2015-08-31 15:53:07 -0400

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'practicalmeteor:mocha-core',
   summary: "Fibers aware mocha server side wrappers. Internal package - use practicalmeteor:mocha.",
-  version: "0.1.1",
+  version: "0.1.2",
   debugOnly: true,
   git: "https://github.com/practicalmeteor/meteor-mocha-core.git"
 });
