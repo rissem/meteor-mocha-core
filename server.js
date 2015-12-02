7fde7b09252be603bd945c7bd4fca9818ab1fce0
Juan Sepulveda <juan@lavaina.com>
2015-12-01 21:47:10 -0400

    Refactor and cleanup code

--- server.js
+++ server.js
@@ -1,59 +1,9 @@
 var Fiber = Npm.require("fibers");
 
 setupGlobals = function(mocha){
-  //basically a direct copy from meteor/packages/meteor/dynamics_nodejs.js
-  //except the wrapped function has an argument (mocha distinguishes
-  //asynchronous tests from synchronous ones by the "length" of the
-  //function passed into it, before, etc.)
-  var moddedBindEnvironment = function (func, onException, _this) {
-    Meteor._nodeCodeMustBeInFiber();
-
-    var boundValues = _.clone(Fiber.current._meteor_dynamics || []);
-
-    if (!onException || typeof(onException) === 'string') {
-      var description = onException || "callback of async function";
-      onException = function (error) {
-        Meteor._debug(
-          "Exception in " + description + ":",
-          error && error.stack || error
-        );
-      };
-    }
-
-    //IMPORTANT note the callback variable present here, for
-    //Metoer.bindEnvironment this is ` return function (/* arguments */) {`
-    return function (callback) {
-      var args = _.toArray(arguments);
-      // If _this is not provided, then function 'this' will be used,
-      // which is the mocha suite context in 'it', 'beforeEach', etc.
-      _this = _this || this;
-
-      var runWithEnvironment = function () {
-        var savedValues = Fiber.current._meteor_dynamics;
-        try {
-          // Need to clone boundValues in case two fibers invoke this
-          // function at the same time
-          Fiber.current._meteor_dynamics = _.clone(boundValues);
-          var ret = func.apply(_this, args);
-        } catch (e) {
-          onException(e);
-        } finally {
-          Fiber.current._meteor_dynamics = savedValues;
-        }
-        return ret;
-      };
-
-      if (Fiber.current)
-        return runWithEnvironment();
-      Fiber(runWithEnvironment).run();
-    };
-  };
-
-
 
   var mochaExports = {};
   mocha.suite.emit("pre-require", mochaExports, undefined, mocha);
-  //console.log(mochaExports);
 
   // 1. patch up it and hooks functions so it plays nice w/ fibers
   // 2. trick to allow binding the suite instance as `this` value
@@ -74,36 +24,39 @@ setupGlobals = function(mocha){
     //fiber, but it was unclear to me how that could be done without
     //forking mocha itself.
 
-    var wrappedFunc = function (callback) {
 
-      if (func.length == 0) {
-        func.call(this);
-        callback();
+    var wrappedFunction = function (done) {
+      var self = this;
+      var run = function() {
+        try {
+          if (func.length == 0) {
+            func.call(self);
+            done();
+          }
+          else {
+            func.call(self, done);
+          }
+        } catch (error) {
+          done(error);
+        }
+      };
+
+      if (Fiber.current) {
+        return run();
       }
-      else {
-        func.call(this, callback);
-      }
+      Fiber(run).run();
     };
 
-    var boundWrappedFunction = moddedBindEnvironment(wrappedFunc, function(err){
-      throw err;
-    });
-
-    // Show original function source code
-    boundWrappedFunction.toString = function(){return func.toString()};
-    return boundWrappedFunction
+    // Show original's function source code
+    wrappedFunction.toString = function(){return func.toString()};
+    return wrappedFunction;
   };
 
-  global.describe = function (name, func){
-    return mochaExports.describe(name, moddedBindEnvironment(func, function(err) { throw err; }), this);
-  };
+  global.describe = mochaExports.describe;
   global.describe.skip = mochaExports.describe.skip;
-  global.describe.only = function(name, func) {
-    mochaExports.describe.only(name, moddedBindEnvironment(func, function(err) { throw err; }), this);
-  };
+  global.describe.only = mochaExports.describe.only;
 
   global['it'] = function (name, func){
-    
     // You can create pending tests without a callback
     // http://mochajs.org/#pending-tests
     // i.e pending test
